513ea638e34dcbe1a638ae48c628702b5baa5df9
Hshafal <hshafal1@yahoo.com>
2025-01-29 01:22:39 +0300

    first commit

---
+++ src/routes/user.route.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+const router = express.Router();
+
+router.post("/login", async (req: Request, res: Response) => {
+	const { password } = req.body;
+
+	// Check the password
+	const isMatch = password == process.env.PASSWORD;
+
+	if (!isMatch) {
+		res.status(400).send({ error: "Invalid password" });
+		return;
+	}
+
+	// Generate a JWT
+	const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET!, {
+		expiresIn: "1h",
+	});
+
+	res.send({ token });
+});
+
+export { router as adminRouter };
